19d973f85bfe594de093c9d7ee5f0ef8d46b5f10
sheetaljwl795 <sheetaljwl795@gmail.com>
2022-12-03 08:23:32 -0800

    Added login,signup mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,60 +4,33 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
-    // users: async () => {
-    //   return User.find().populate('thoughts');
-    // },
-    // user: async (parent, { username }) => {
-    //   return User.findOne({ username }).populate('thoughts');
-    // },
-    // thoughts: async (parent, { username }) => {
-    //   const params = username ? { username } : {};
-    //   return Thought.find(params).sort({ createdAt: -1 });
-    // },
-    // thought: async (parent, { thoughtId }) => {
-    //   return Thought.findOne({ _id: thoughtId });
-    // },
     me: async (parent, args, context) => {
       if (context.user) {
-        return User.findOne({ _id: context.user._id }).populate('books');
+        return User.findOne({ _id: context.user._id }).select("-__v -password").populate("savedBooks");
       }
       throw new AuthenticationError('You need to be logged in!');
     },
-    users: async(parent,args) => {
-      return User.find()
-    }
   },
 
   Mutation: {
+    
     addUser: async (parent, { username, email, password }) => {
-      console.log(username, email, password)
-try{
-  const user = await User.create({ username, email, password });
-  // const token = signToken(user);
-  // return { token, user };
-  return user
-
-}
-catch (error){
-  console.log(error)
-}
-
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
     },
+   
+
     login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
-
       if (!user) {
         throw new AuthenticationError('No user found with this email address');
       }
-
       const correctPw = await user.isCorrectPassword(password);
-
       if (!correctPw) {
         throw new AuthenticationError('Incorrect credentials');
       }
-
       const token = signToken(user);
-
       return { token, user };
     },
   
